7c61ee5902feb7c704963faa8ffa761492b7fa4d
alfndp <alfndp25@gmail.com>
2024-10-09 21:27:25 +0900

    기본 틀 잡기

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import LOGO from "./assets/bomjel_logo.png";
+import {CALCULATOR} from "./util/investment.js";
+
 function App() {
+  CALCULATOR();
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <header>
+        <img src={LOGO} alt="logo_Img"/>
+        <h1>B&J Stock Tracker</h1>
+      </header>
+      <div id="user-input">
+        <label htmlFor="input1">INITIAL INVESTMENT</label>
+        <input type="text" id="input1"/>
+        <label htmlFor="input2">ANNUAL INVESTMENT</label>
+        <input type="text" id="input2"/>
+        <label htmlFor="input3">EXPECTED RETURN</label>
+        <input type="text" id="input3"/>
+        <label htmlFor="input4">DURATION</label>
+        <input type="text" id="input4"/>
+      </div>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest(Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+
+    </>
   )
 }
 
---
+++ src/todo.md
@@ -0,0 +1,8 @@
+# todo list
+1. 상단에 이미지와 타이틀
+2. 그 밑에 사용자 input 영역
+(INITIAL INVESTMENT, ANNUAL INVESTMENT, EXPECTED RETURN, DURATION,)
+3. 그 밑에 input 값에 따른 계산 결과를 테이블 형태로 보여줌
+4. calculateInvestmentResults 외부 함수 사용하기
+5. 테이블에 결과 나오도록 하기 
+6. component 분리하기
